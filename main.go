6752367dc12e556fba75b43c1b65b85d9f54d67e
Jordan Buckley <149504654+JordanBuckleyGit@users.noreply.github.com>
2024-10-08 19:53:19 +0100

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
     "net/http"
 )
 
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
+}
+    
 func main() {
     http.HandleFunc("/", helloWorld)
     fmt.Println("Server starting on port 8080...")
