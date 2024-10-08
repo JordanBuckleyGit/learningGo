e9e09d19b29c4811bf9bb053147acaba3faa33de
Jordan Buckley <149504654+JordanBuckleyGit@users.noreply.github.com>
2024-10-08 19:52:02 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,3 +4,9 @@ import (
     "fmt"
     "net/http"
 )
+
+func main() {
+    http.HandleFunc("/", helloWorld)
+    fmt.Println("Server starting on port 8080...")
+
+    err := http.ListenAndServer(":8080",nil)
